71dc84214a99cc07cc8db0484e0c1bed5f79d2c2
hangillee <skfcb10@naver.com>
2023-01-27 18:39:59 +0900

    feat: implement join and login api

---
+++ src/main/java/gdsc/skhu/dalbit/domain/DTO/LoginDTO.java
@@ -0,0 +1,9 @@
+package gdsc.skhu.dalbit.domain.DTO;
+
+import lombok.Data;
+
+@Data
+public class LoginDTO {
+    private String username;
+    private String password;
+}
---
+++ src/main/java/gdsc/skhu/dalbit/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package gdsc.skhu.dalbit.repository;
+
+import gdsc.skhu.dalbit.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUsername(String username);
+}
