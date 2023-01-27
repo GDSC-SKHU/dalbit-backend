7ad84d3182679f64765dfe27fd147150fef8e696
hangillee <skfcb10@naver.com>
2023-01-27 21:31:35 +0900

    chore: edit code with convention

--- src/main/java/gdsc/skhu/dalbit/controller/MemoController.java
+++ src/main/java/gdsc/skhu/dalbit/controller/MemoController.java
@@ -6,14 +6,12 @@ import gdsc.skhu.dalbit.domain.Memo;
 import gdsc.skhu.dalbit.service.MemoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class MemoController {
     private final MemoService memoService;
