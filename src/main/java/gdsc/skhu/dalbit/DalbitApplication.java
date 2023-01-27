e1489286a2a8f87aa348dbbc9cb621b9f2632618
hangillee <skfcb10@naver.com>
2023-01-27 16:11:06 +0900

    init commit

---
+++ src/main/java/gdsc/skhu/dalbit/DalbitApplication.java
@@ -0,0 +1,13 @@
+package gdsc.skhu.dalbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DalbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DalbitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gdsc/skhu/dalbit/DalbitApplicationTests.java
@@ -0,0 +1,13 @@
+package gdsc.skhu.dalbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DalbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
